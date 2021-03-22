616b0013ad8eb64d7a1e32ad876007ee4169f698
Ehsanuzzaman <ehsanuzzaman113@gmail.com>
2021-03-22 18:04:52 +0600

    Lab Task 2

---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Ehsan", "2356", 55000);
+            Account a2 = new Account("Zaman", "9872", 12000);
+            a1.showInfo();
+            a2.showInfo();
+            a1.Withdraw(1000);
+            a1.Deposit(12000);
+            a1.Transfer(a2, 5000);
+            a1.showInfo();
+            a2.showInfo();
+            Transaction t1 = new Transaction(a1, a2, 6000, "Business perpose");
+            t1.showInfo();
+        }
+    }
+}
---
+++ Lab 2/Lab 2/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Transaction : Account
+    {
+        public Account sender { get; set; }
+        public Account receiver { get; set; }
+        public int amount { get; set; }
+        public string additionalInfo { get; set; }
+
+        public Transaction()
+        {
+            Console.WriteLine(" Transaction Deafult.");
+        }
+        public Transaction(Account sender, Account receiver, int amount, string additionalInfo)
+        {
+            this.sender = sender;
+            this.receiver = receiver;
+            this.amount = amount;
+            this.additionalInfo = additionalInfo;
+        }
+
+        override public void showInfo()
+        {
+            Console.WriteLine("Sender is: " + sender.AccName);
+            Console.WriteLine("Receiver is: " + receiver.AccName);
+            Console.WriteLine("Amount is: " + amount);
+            Console.WriteLine("Additional Info: " + additionalInfo);
+        }
+    }
+}
